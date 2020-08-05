4ce599e7d346489cd93e5bebca46a7a027d56071
Shoeyuz <michael.shlega@gmail.com>
2020-08-05 09:33:02 -0400

    creating of cards displaying info from php

--- html/main.php
+++ html/main.php
@@ -16,18 +16,19 @@
     if (!$conn) {
        die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connectedh successfully";
+    //echo "Connectedh successfully";
 
 
     //---------------------------------------------------
     //             0 IS MEN ---- 1 IS WOMEN for gender
     //             0 is game ---- 1 IS TOURNAMENT for tournament
+    //             0 is JR ------- 1 is SENIOR
     //-----------------------------------------------------
    
    
     //query for all games
    
-    $sql = "SELECT id, place, timing, tournament, gender
+    $sql = "SELECT id, place, timing, tournament, gender, age
     FROM Games ORDER BY id";
     
 
@@ -45,4 +46,63 @@
     
 ?>
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Assignments</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+  </head>
+  <body>
+  
+
+
+  <section class = "games">
+
+    <?php foreach($assignments as $assignment){ ?>
+        <div class="card">
+            <header class="card-header">
+                <p class="card-header-title">
+                <?php echo htmlspecialchars($assignment['place']);?>
+
+                </p>
+                
+            </header>
+        <div class="card-content">
+            <div class="content">
+                <?php 
+                    if($assignment['gender'])
+                        echo ("Girls ");
+                    else
+                        echo ("Boys ");
+
+                    if($assignment['age'])
+                        echo ("Senior ");
+                    else
+                        echo ("Junior ");
+                ?>
+                <br>
+                <?php 
+                    if($assignment['tournament'])
+                        echo ("Tournament ");
+                    else
+                        echo ("Game ");
+                ?>
+                <br>
+                <time datetime="2016-1-1"><?php echo htmlspecialchars($assignment['timing']);?></time>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" id="request" class="card-footer-item">Request Assignment</a>
+        </footer>
+        </div>
+
+    <?php }?>
+
+  </section>
+  
+ 
+  </body>
+</html>
 
