ed226d133b765c42d8bc9426e204cc2689c3f32a
Shoeyuz <michael.shlega@gmail.com>
2020-08-13 09:49:21 -0400

    request js and modal

--- html/main.php
+++ html/main.php
@@ -1,74 +1,75 @@
 <?php
 
-    //gets all the error reporting, comment out when live
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    //
-
-    
-    $servername = "localhost";
-    $username = "michael";
-    $password = "root";
-    $db = "arbiter_system";
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password,$db);
-    // Check connection
-    if (!$conn) {
-       die("Connection failed: " . mysqli_connect_error());
-    }
-    //echo "Connectedh successfully";
+//gets all the error reporting, comment out when live
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+//
 
 
-    //---------------------------------------------------
-    //             0 IS MEN ---- 1 IS WOMEN for gender
-    //             0 is game ---- 1 IS TOURNAMENT for tournament
-    //             0 is JR ------- 1 is SENIOR
-    //-----------------------------------------------------
-   
-   
-    //query for all games
-   
-    $sql = "SELECT id, place, timing, tournament, gender, age
+$servername = "localhost";
+$username = "michael";
+$password = "root";
+$db = "arbiter_system";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connectedh successfully";
+
+
+//---------------------------------------------------
+//             0 IS MEN ---- 1 IS WOMEN for gender
+//             0 is game ---- 1 IS TOURNAMENT for tournament
+//             0 is JR ------- 1 is SENIOR
+//-----------------------------------------------------
+
+
+//query for all games
+
+$sql = "SELECT id, place, timing, tournament, gender, age
     FROM Games ORDER BY timing ASC";
-    
-
-    //make querry and get result
-    $result = mysqli_query($conn,$sql);
-
-    //fetch the rows
-    $assignments = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-    //free and clear memory with connection
-    mysqli_free_result($result);    
 
 
+//make querry and get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the rows
+$assignments = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free and clear memory with connection
+mysqli_free_result($result);
 
 
 
 
 
 
-    //end connection
-    mysqli_close($conn);
-    //print_r($assignments);
 
-    
+
+//end connection
+mysqli_close($conn);
+//print_r($assignments);
+
+
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assignments</title>
-    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-    <link rel="stylesheet" href = "/Volleyball_Arbiter_System/css/main.css">
-    <script src="/Volleyball_Arbiter_System/js/main.js"></script> 
-  </head>
 
- <!-- NAVIGATION -->
- <nav class="navbar" role="navigation" aria-label="main navigation">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Assignments</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+  <link rel="stylesheet" href="/Volleyball_Arbiter_System/css/main.css">
+  <script src="/Volleyball_Arbiter_System/js/main.js"></script>
+</head>
+
+<!-- NAVIGATION -->
+<nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
@@ -83,11 +84,11 @@
 
   <div class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href = main.php>
+      <a class="navbar-item" href=main.php>
         Assignment List
       </a>
 
-      <a class="navbar-item" href = "rules.php">
+      <a class="navbar-item" href="rules.php">
         League Rules
       </a>
 
@@ -140,71 +141,98 @@
 
 
 
- <!-- CARDS SHOWING -->
-  <section class = "games">
+  <!-- CARDS SHOWING -->
+  <section class="games">
 
-    <?php foreach($assignments as $assignment){ ?>
-        <div class="card" id = <?php echo $assignment['id'] ?>>
+    <?php foreach ($assignments as $assignment) { ?>
+      <div class="card" id=<?php echo $assignment['id'] ?>>
         <!-- this sets the id of the div to the game id -->
-        
-            <header class="card-header">
-                <p class="card-header-title">
-                <?php echo htmlspecialchars($assignment['place']);?>
 
-                </p>
-                
-            </header>
-          <div class="card-content">
-              <div class="content">
-                  <?php 
-                      if($assignment['gender'])
-                          echo ("Girls ");
-                      else
-                          echo ("Boys ");
+        <header class="card-header">
+          <p class="card-header-title">
+            <?php echo htmlspecialchars($assignment['place']); ?>
 
-                      if($assignment['age'])
-                          echo ("Senior ");
-                      else
-                          echo ("Junior ");
-                  ?>
-                  <br>
-                  <?php 
-                      if($assignment['tournament'])
-                          echo ("Tournament ");
-                      else
-                          echo ("Game ");
-                  ?>
-                  <br>
-                  <time datetime="2016-1-1"><?php echo htmlspecialchars($assignment['timing']);?></time>
-              </div>
-          </div>
-          <footer class="card-footer">
-              <a href="#" id="request" class="card-footer-item">Request Assignment</a>
-          </footer>
-          <!-- MODAL -->
-          <div class="modal" id="assignment-application">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-              <header class="modal-card-head">
-                <p class="modal-card-title">Modal title</p>
-                <button class="delete" aria-label="close"></button>
-              </header>
-              <section class="modal-card-body">
-                <!-- Content ... -->
-              </section>
-              <footer class="modal-card-foot">
-                <button class="button is-success">Save changes</button>
-                <button class="button">Cancel</button>
-              </footer>
-            </div>
+          </p>
+
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <?php
+            if ($assignment['gender'])
+              echo ("Girls ");
+            else
+              echo ("Boys ");
+
+            if ($assignment['age'])
+              echo ("Senior ");
+            else
+              echo ("Junior ");
+            ?>
+            <br>
+            <?php
+            if ($assignment['tournament'])
+              echo ("Tournament ");
+            else
+              echo ("Game ");
+            ?>
+            <br>
+            <time datetime="2016-1-1"><?php echo htmlspecialchars($assignment['timing']); ?></time>
           </div>
         </div>
+        <footer class="card-footer">
+          <a href="#" id="request" class="card-footer-item">Request Assignment</a>
+        </footer>
+        <!-- MODAL -->
+        <div class="modal" id="assignment-application">
+          <div class="modal-background"></div>
+          <div class="modal-card">
 
-    <?php }?>
+            <header class="modal-card-head">
+              <p class="modal-card-title">Modal title</p>
+              <button class="delete" aria-label="close"></button>
+            </header>
+
+            <!-- this is the form -->
+            <form id = "submission">
+            <section class="modal-card-body">
+              <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="e.g Alex Smith">
+                </div>
+              </div>
+
+
+              <div class="field">
+                <label class="label">Request Code</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="e.g 1XQJS54">
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                  <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+                </div>
+              </div>
+            </section>
+            </form>
+            <!-- this is the end form -->
+
+            <footer class="modal-card-foot">
+              <button class="button is-success">Save changes</button>
+              <button class="button is-cancel">Cancel</button>
+            </footer>
+          </div>
+        </div>
+      </div>
+
+    <?php } ?>
 
   </section>
-  
- 
-  </body>
-</html>
 
+
+</body>
+
+</html>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 
-var button = document.getElementsByClassName('card-footer-item');
-var modal = document.getElementById('assignment-application');
 
 
-button.onclick = function() {
-  modal.style.display = 'block';
 
-}
+//Function purpose: Wait for load
+//
+document.addEventListener("DOMContentLoaded", function (e) {
+  var button = document.getElementsByClassName('card-footer-item');
+  const modal = document.querySelector('.modal');
+  const cancel = document.querySelector('.delete');
+  const close = document.querySelector('.is-cancel')
+  const submit = document.querySelector('.is-success')
+
+
+  var i;
+  for (i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+      modal.style.display = "block";
+    });
+  }
+
+
+
+
+  close.addEventListener('click', function () {
+    modal.style.display = 'none';
+    resetForms
+  });
+
+  cancel.addEventListener('click', function () {
+    modal.style.display = 'none';
+    resetForms
+
+  });
+
+
+
+
+
+
+
+  /*
+     Resets form of modal
+     Not working 
+  */
+  function resetForms() {
+    document.getElementById("submission").reset();
+  }
+});
+
+
